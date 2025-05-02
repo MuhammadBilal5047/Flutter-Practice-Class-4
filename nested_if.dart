59373dba9d4744d83ef8c7e284e56c6786610e85
MuhammadBilal5047 <mbmyvadiwala@gmail.com>
2025-05-02 23:13:19 +0500

    Update nested-if.dart

--- nested_if.dart
+++ nested_if.dart
@@ -1,15 +1,14 @@
 void main() {
-   
-  int number1 = 20;
-  if (number1 % 5 == 0 || number1 % 7 == 0) {
-    if (number1 % 5 == 0) {
-      print("number1 is divisible by 5");
-    } else if (number1 % 7 == 0) {
-      print("numbe1r is divisible by 7");
-    } else {
+  int number = 35;
+  if (number % 5 == 0 || number % 7 == 0) {
+    if (number % 5 == 0 && number%7==0) {
       print("number is divisible by both");
+    } else if (number % 7 == 0) {
+      print("number is divisible by 7");
+    } else {
+      print("number is divisible by 5");
     }
   } else {
-    print("number1 is not divisible by 5 or 7");
+    print("number is not divisible by 5 or 7");
   }
 }
